50fe97ad0b8085abc6c8128dbe80cd30e79e84d0
Alex <alexwisco29@gmail.com>
2024-12-09 13:31:03 -0500

    reformatted with vsc extension

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,47 +1,45 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = '/api/notes'
-
-
+const baseUrl = "/api/notes";
 
 // called on page start up to get everyone in the db
 const getAll = () => {
   //return axios.get(baseUrl)
-  const request = axios.get(baseUrl)
-  return request.then(response => {
-    return response.data
-    //promise returned by axios no longer returned directly. Promise is 
+  const request = axios.get(baseUrl);
+  return request.then((response) => {
+    return response.data;
+    //promise returned by axios no longer returned directly. Promise is
     // taken to the request variable, and the 'then' function is called for it.
     // getAll() now returns a promise, because the 'then' of the 'promise' returns a promise
-
-  })
-}
+  });
+};
 
 // adding a new person to the phonebook.
-const create = newObject => {
+const create = (newObject) => {
   //return axios.post(baseUrl)
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => {
-    return response.data
-  })
-}
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => {
+    return response.data;
+  });
+};
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
-const noteDelete = (id) => { // delete a note given the notes id value (change?)
-    return axios.delete(`${baseUrl}/${id}`)
-}
+const noteDelete = (id) => {
+  // delete a note given the notes id value (change?)
+  return axios.delete(`${baseUrl}/${id}`);
+};
 
-export default { getAll, create, update, noteDelete }
+export default { getAll, create, update, noteDelete };
 
-  // or 
+// or
 
-  /* 
+/* 
 getAll: getAll, 
   create: create, 
   update: update 
   */
- // Since the name of the fields of the object are 
- // the same as the names of the vars defining their value, can short form it
+// Since the name of the fields of the object are
+// the same as the names of the vars defining their value, can short form it
